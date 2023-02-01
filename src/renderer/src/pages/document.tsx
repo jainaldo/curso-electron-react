aa5962475a3c325d24fc3bda25fe4802dee891c2
jainaldo <jainaldosilva@gmail.com>
2023-02-01 09:40:05 -0300

    construcaoda aplicacao: pagina do documento

--- src/renderer/src/pages/document.tsx
+++ src/renderer/src/pages/document.tsx
@@ -1,12 +1,24 @@
-import { Link } from 'react-router-dom'
+import { ToC } from '../components/ToC'
 
 export function Document() {
   return (
-    <main className="flex-1 flex items-center justify-center text-rotion-400">
-      document
-      <p>
-        <Link to="/">acessar home</Link>
-      </p>
+    <main className="flex-1 flex py-12 px-10 gap-8">
+      <aside className="hidden lg:block sticky top-0">
+        <span className="text-rotion-300 font-semibold text-xs">
+          TABLE OF CONTENTS
+        </span>
+        <ToC.Root>
+          <ToC.Link>Backend</ToC.Link>
+          <ToC.Section>
+            <ToC.Link>Banco de dados</ToC.Link>
+            <ToC.Link>Autenticação</ToC.Link>
+          </ToC.Section>
+        </ToC.Root>
+      </aside>
+
+      <section className="flex-1 flex flex-col items-center">
+        asdasdasdasd
+      </section>
     </main>
   )
 }
