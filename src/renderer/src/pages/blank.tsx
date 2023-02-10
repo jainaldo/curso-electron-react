9a932b0b36671866265a1ad5b65c20d4c9547218
jainaldo <jainaldosilva@gmail.com>
2023-02-09 22:17:52 -0300

    Storage e comunicação: Visualização do documento

--- src/renderer/src/pages/blank.tsx
+++ src/renderer/src/pages/blank.tsx
@@ -1,11 +1,7 @@
-import { Link } from 'react-router-dom'
 export function Blank() {
   return (
     <main className="flex-1 flex items-center justify-center text-rotion-400">
       Selecione ou crie um documento
-      <p>
-        <Link to="/document">acessar documento</Link>
-      </p>
     </main>
   )
 }
